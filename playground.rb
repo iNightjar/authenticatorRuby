0190d9273106432dc5b54d05c94c8f30003bb773
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-05-13 20:09:07 +0300

    logic thinking and steps of authenticator code

---
+++ playground.rb
@@ -0,0 +1,19 @@
+users = [
+          { username: "inightjar", password: "password1" },
+          { username: "jack", password: "password2" },
+          { username: "arya", password: "password3" },
+          { username: "jonshow", password: "password4" },
+          { username: "heisenberg", password: "password5" }
+        ]
+puts "welcome to the authenticator! "
+25.times { print "_"}
+puts
+puts "this program will take input from the user and compare password"
+puts "if password is correct, you will get back the user object"
+
+# hash, array, branching, while loops and designing program execution flow
+attemps = 1
+while attemps < 4
+    puts "this will keep printing!"
+    attemps += 1
+end
