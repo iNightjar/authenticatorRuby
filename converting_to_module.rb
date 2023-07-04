791fc7ea6bfd21308d140b9ac0c75eee19a4182e
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-07-04 07:12:25 +0300

    bcrypt module to check users credentails while it's encrypted

---
+++ converting_to_module.rb
@@ -0,0 +1,41 @@
+require 'bcrypt'
+
+users = [
+  { username: "mashrur", password: "password1" },
+  { username: "jack", password: "password2" },
+  { username: "arya", password: "password3" },
+  { username: "jonshow", password: "password4" },
+  { username: "heisenberg", password: "password5" }
+]
+
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+      return user_record
+    end
+  end
+  "Credentials were not correct"
+end
+
+new_users = create_secure_users(users)
+# puts new_users
+
+# user iNightjar is not in the users list
+puts authenticate_user("iNightjar", "password6", users) # PRINTS: Credentails were not correct
